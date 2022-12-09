93aca3c2f7d6808737d5e5499622366114f45a3a
mohamedtharwat000 <81764836+mohamedtharwat000@users.noreply.github.com>
2022-12-09 15:17:52 +0200

    responsive design completed

---
+++ assets/js/main.js
@@ -0,0 +1,5 @@
+const userName = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmpassword");
+const signUp = document.getElementById("signup");
